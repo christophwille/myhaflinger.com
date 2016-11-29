0cf853170c89217493e1332aa2416616668b134d
Christoph Wille <christoph.wille@gmail.com>
2016-11-29 17:16:44 +0100

    Authentication wedged on top of Web Pages

---
+++ site/App_Code/Anmeldung/AuthManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace MyHaflinger.Anmeldung
+{
+    public static class AuthenticationFactory
+    {
+        public static ClaimsIdentity CreateIdentity(string username, string role)
+        {
+            var claimsIdentity = new ClaimsIdentity(DefaultAuthenticationTypes.ApplicationCookie, ClaimTypes.Name, ClaimTypes.Role);
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.NameIdentifier, username));
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, username));
+            claimsIdentity.AddClaim(
+                new Claim("http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider",
+                        "ASP.NET Identity",
+                        "http://www.w3.org/2001/XMLSchema#string"));
+
+            var adminClaim = new Claim(ClaimTypes.Role, role);
+            claimsIdentity.AddClaim(adminClaim);
+
+            return claimsIdentity;
+        }
+    }
+}
---
+++ site/App_Code/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        app.UseCookieAuthentication(new CookieAuthenticationOptions
+        {
+            AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+            LoginPath = new PathString("/Anmeldung/Login")
+        });
+        // Use a cookie to temporarily store information about a user logging in with a third party login provider
+        app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+    }
+}
