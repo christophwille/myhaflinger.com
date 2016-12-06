394f9586851e905d086eed224387654c46a5670f
Christoph Wille <christoph.wille@gmail.com>
2016-12-06 11:17:49 +0100

    Start Formular

--- site/App_Code/Anmeldung/Data.Context.cs
+++ site/App_Code/Anmeldung/Data.Context.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using SQLite;
+using System.Linq.Expressions;
 
 namespace MyHaflinger.Anmeldung.Data
 {
+    // https://github.com/praeclarum/sqlite-net
     public static class DbFactory
     {
         public static AnmeldungsDbContext CreateContext(HttpServerUtilityBase server)
@@ -29,6 +31,18 @@ namespace MyHaflinger.Anmeldung.Data
                 ChallengeGuid = guid,
                 ChallengeRequested = registrationTime
             });
+        }
+
+        public string GetEmailForChallengeToken(string token)
+        {
+            var challenge = Table<EmailChallenge>().Where(c => c.ChallengeGuid == token).FirstOrDefault();
+
+            if (null != challenge)
+            {
+                return challenge.EmailAddress;
+            }
+
+            return "";
         }
     }
 }
