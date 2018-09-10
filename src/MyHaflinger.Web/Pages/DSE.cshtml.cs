c6e654637a781ea8ad5134b6ff944fa5abc983a8
Christoph Wille <christoph.wille@gmail.com>
2018-09-10 13:52:15 +0200

    Add page model for DSE

---
+++ src/MyHaflinger.Web/Pages/DSE.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyHaflinger.Web.Common;
+
+namespace MyHaflinger.Web.Pages
+{
+	public class DSEModel : MyHaflingerBasePageModel
+	{
+		public void OnGet()
+		{
+		}
+	}
+}
