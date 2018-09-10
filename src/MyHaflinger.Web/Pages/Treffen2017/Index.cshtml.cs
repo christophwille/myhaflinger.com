3454c89348657a7954e3b04252b9fba45b9e7e31
Christoph Wille <christoph.wille@gmail.com>
2018-09-10 09:05:44 +0200

    Cache Treffen2017 page

--- src/MyHaflinger.Web/Pages/Treffen2017/Index.cshtml.cs
+++ src/MyHaflinger.Web/Pages/Treffen2017/Index.cshtml.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyHaflinger.Web.Common;
 
 namespace MyHaflinger.Web.Pages.Treffen2017
 {
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
+	public class IndexModel : MyHaflingerBasePageModel
+	{
+		public void OnGet()
+		{
 
-        }
-    }
+		}
+	}
 }
