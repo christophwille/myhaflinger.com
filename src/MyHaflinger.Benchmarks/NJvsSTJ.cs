10641e96faa7d2f3ef0166ae269aa4b0d35b998f
Christoph Wille <christoph.wille@gmail.com>
2019-07-31 15:45:20 +0200

    Add simple benchmarking

---
+++ src/MyHaflinger.Benchmarks/NJvsSTJ.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace MyHaflinger.Benchmarks
+{
+	public class LogonUser
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public string Role { get; set; }
+	}
+
+	[MemoryDiagnoser] // https://adamsitnik.com/the-new-Memory-Diagnoser/
+	public class NJvsSTJ
+	{
+		private readonly string _jsonFilePath = "AnmeldungAccounts.json";
+
+		[Benchmark]
+		public void NJ()
+		{
+			string fileContents = File.ReadAllText(_jsonFilePath);
+			var definedAccounts = Newtonsoft.Json.JsonConvert.DeserializeObject<List<LogonUser>>(fileContents);
+		}
+
+		[Benchmark]
+		public async Task STJAsync()
+		{
+			using (var stream = File.OpenRead(_jsonFilePath))
+			{
+				var options = new JsonSerializerOptions
+				{
+					AllowTrailingCommas = true,
+					PropertyNameCaseInsensitive = true
+				};
+
+				var definedAccounts = await System.Text.Json.JsonSerializer.DeserializeAsync<List<LogonUser>>(stream, options);
+				stream.Close();
+			}
+		}
+
+		[Benchmark]
+		public void STJ()
+		{
+			string fileContents = File.ReadAllText(_jsonFilePath);
+
+			var options = new JsonSerializerOptions
+			{
+				AllowTrailingCommas = true,
+				PropertyNameCaseInsensitive = true
+			};
+
+			var definedAccounts = System.Text.Json.JsonSerializer.Deserialize<List<LogonUser>>(fileContents, options);
+		}
+	}
+}
---
+++ src/MyHaflinger.Benchmarks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace MyHaflinger.Benchmarks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<NJvsSTJ>();
+			/*
+				|   Method |        Mean |     Error |    StdDev |   Gen 0 |  Gen 1 | Gen 2 | Allocated |
+				|--------- |------------:|----------:|----------:|--------:|-------:|------:|----------:|
+				|       NJ |    62.33 us |  1.214 us |  1.247 us |  5.6152 |      - |     - |  11.63 KB |
+				| STJAsync | 1,128.95 us | 22.195 us | 22.793 us |  9.7656 | 3.9063 |     - |  20.99 KB |
+				|      STJ | 1,063.56 us | 17.263 us | 16.148 us | 13.6719 | 5.8594 |     - |   29.4 KB |
+			 */
+		}
+	}
+}
