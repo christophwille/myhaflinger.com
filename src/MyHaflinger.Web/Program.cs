5874326808978f9266df327d9461cc04d9b7ce42
Christoph Wille <christoph.wille@gmail.com>
2019-07-31 09:46:33 +0200

    Basic migration based on https://docs.microsoft.com/en-us/aspnet/core/migration/22-to-30?view=aspnetcore-2.2&tabs=visual-studio
    RazorLight issue https://github.com/toddams/RazorLight/issues/258 also happens here (email controller works per se, but templating crashes as outlined in issue in repo)
    
    Microsoft.AspNetCore.Authentication.Cookies has no 3.0 version on NuGet yet (Common)

--- src/MyHaflinger.Web/Program.cs
+++ src/MyHaflinger.Web/Program.cs
@@ -17,12 +17,11 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace MyHaflinger.Web
@@ -31,12 +30,14 @@ namespace MyHaflinger.Web
 	{
 		public static void Main(string[] args)
 		{
-			CreateWebHostBuilder(args).Build().Run();
+			CreateHostBuilder(args).Build().Run();
 		}
 
-		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
-				.UseStartup<Startup>();
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
 	}
 }
-
