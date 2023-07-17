bf81312e64c06b10254cff3e09e7ae2eb283f8d8
Christoph Wille <christoph.wille@gmail.com>
2023-07-17 10:17:43 +0200

    Treffen 2014 aus Forenthread in Website kopiert

--- src/MyHaflinger.Web/Pages/Treffen2014/Index.cshtml.cs
+++ src/MyHaflinger.Web/Pages/Treffen2014/Index.cshtml.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyHaflinger.Web.Pages.Treffen2017
+namespace MyHaflinger.Web.Pages.Treffen2014
 {
 	[ResponseCache(CacheProfileName = "Default")]
 	public class IndexModel : PageModel
