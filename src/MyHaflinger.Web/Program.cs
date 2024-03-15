4466b6c1deec9c78957095cc025e787a5975023f
Christoph Wille <christoph.wille@gmail.com>
2024-03-15 15:09:47 +0100

    Migrate to .NET 8.0

--- src/MyHaflinger.Web/Program.cs
+++ src/MyHaflinger.Web/Program.cs
@@ -1,46 +1,96 @@
-﻿using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 using NLog;
 using NLog.Web;
+using MyHaflinger.Common.Services;
+using MyHaflinger.Web.Models;
+using MyHaflinger.Web.Services;
+using System.Globalization;
 
-namespace MyHaflinger.Web
+var builder = WebApplication.CreateBuilder(args);
+
+var logger = NLog.LogManager.Setup().LoadConfigurationFromFile("nlog.config").GetCurrentClassLogger();
+#if !DEBUG
+	builder.Logging.ClearProviders();
+#endif
+builder.Logging.AddNLogWeb();
+
+// https://dotnetcoretutorials.com/2017/06/22/request-culture-asp-net-core/
+// https://docs.microsoft.com/en-us/aspnet/core/fundamentals/localization?view=aspnetcore-2.1#implement-a-strategy-to-select-the-languageculture-for-each-request
+builder.Services.Configure<RequestLocalizationOptions>(options =>
 {
-	public class Program
+	options.DefaultRequestCulture = new RequestCulture("de");
+	options.SupportedCultures = new List<CultureInfo> { new CultureInfo("de") };
+	options.SupportedUICultures = new List<CultureInfo> { new CultureInfo("de") };
+	options.RequestCultureProviders = new List<IRequestCultureProvider>();
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+	.AddCookie(options =>
 	{
-		// https://github.com/NLog/NLog/wiki/Getting-started-with-ASP.NET-Core-2
-		public static void Main(string[] args)
-		{
-			var logger = NLog.LogManager.Setup().LoadConfigurationFromFile("nlog.config").GetCurrentClassLogger();
+		options.LoginPath = new PathString("/Anmeldung/Verwaltung/Login");
+	});
 
-			try
-			{
-				logger.Debug("Starting program");
-				CreateHostBuilder(args).Build().Run();
-			}
-			catch (Exception ex)
-			{
-				logger.Error(ex, "Stopped program because of exception");
-				throw;
-			}
-			finally
-			{
-				NLog.LogManager.Shutdown();
-			}
-		}
+builder.Services.AddResponseCaching();
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				})
-				.ConfigureLogging(logging =>
-				{
-					logging.ClearProviders();
-					logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-				})
-				.UseNLog();
-	}
+builder.Services.AddRazorPages()
+	.AddRazorRuntimeCompilation();
+
+builder.Services.Configure<MvcOptions>(options =>
+{
+	options.Filters.Add(new RequireHttpsAttribute { Permanent = true });
+	// options.ReturnHttpNotAcceptable = false;
+
+	// https://docs.microsoft.com/en-us/aspnet/core/performance/caching/response?view=aspnetcore-3.1#responsecache-attribute
+	options.CacheProfiles.Add("Default",
+	new CacheProfile()
+	{
+		Duration = 3600,
+		Location = ResponseCacheLocation.Client
+	});
+});
+
+var ao = builder.Configuration.GetSection("AppOptions").Get<AppOptions>();
+
+builder.Services.AddSingleton(ao);
+builder.Services.AddSingleton<ISmtpConfiguration>(ao);
+
+builder.Services.AddTransient<AnmeldungsDbFactory>();
+builder.Services.AddTransient<AnmeldungsAuthenticationFactory>();
+builder.Services.AddTransient<RegistrationFlowAuditTrailService>();
+
+builder.Services.AddTransient<ISmtpMailService, SmtpMailService>();
+builder.Services.AddTransient<ITemplateRenderingService, HandlebarsRenderingService>();
+builder.Services.AddTransient<ContactUsService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	// app.UseHsts();
 }
+
+app.UseRequestLocalization();
+
+// app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseResponseCaching();
+
+app.MapRazorPages();
+
+app.MapPost("/mailfunc", async ([FromBody] MailJson mm, [FromServices] ContactUsService contactService) =>
+{
+	bool ok = await contactService.SendAnfrageFormularMail(mm);
+
+	return Results.Ok();
+});
+
+app.Run();
---
+++ src/MyHaflinger.Web/Services/ContactUsService.cs
@@ -0,0 +1,57 @@
+﻿using MyHaflinger.Common.Services;
+using MyHaflinger.Web.Models;
+
+namespace MyHaflinger.Web.Services
+{
+	public class ContactUsService
+	{
+		private readonly ISmtpMailService _smtpMailService;
+		private readonly ITemplateRenderingService _templateRenderer;
+		private readonly AppOptions _ao;
+		private readonly ILogger<ContactUsService> _logger;
+
+		public ContactUsService(ISmtpMailService smtpMailService, ITemplateRenderingService templateRenderer,
+			AppOptions ao, ILogger<ContactUsService> logger)
+		{
+			_smtpMailService = smtpMailService;
+			_templateRenderer = templateRenderer;
+			_ao = ao;
+			_logger = logger;
+		}
+
+		public async Task<bool> SendAnfrageFormularMail(MailJson mm)
+		{
+			if (string.IsNullOrEmpty(mm.Name) || string.IsNullOrEmpty(mm.Email) ||
+				string.IsNullOrEmpty(mm.Subject) || string.IsNullOrEmpty(mm.Message))
+			{
+				return false;
+			}
+
+			try
+			{
+				string msgToSend = _templateRenderer.Render(AnfrageTemplate, mm);
+
+				return await _smtpMailService.SendMailAsync(_ao.ContactFormTo, "myhaflinger.com Kontaktformular", msgToSend, true, _ao.MailFromAddress);
+
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex.ToString());
+			}
+
+			return true;
+		}
+
+		private static readonly string AnfrageTemplate = @"<!DOCTYPE html>
+		<html>
+		<body>
+			<h1>Anfrage myhaflinger.com</h1>
+			<p>Name: {{Name}}</p>
+			<p>Email: {{Email}}</p>
+			<p>Betreff: {{Subject}}</p>
+			<p>Nachricht:</p>
+			<p>{{Message}}</p>
+		</body>
+		</html>";
+	}
+}
